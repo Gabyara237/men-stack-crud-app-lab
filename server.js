abc22bf66417d6c9a187637d63ca11c27cb86806
Gabriela Araujo <gabyara237@gmail.com>
2025-12-05 12:09:30 -0800

    feat: set up MongoDB connection and server configuration

---
+++ server.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const dotenv = require('dotenv')
+dotenv.config();
+
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on("connected",()=>{
+    console.log(`Connect to MongoDB ${mongoose.connection.name}`);
+})
+
+app.listen('3000', ()=>{
+    console.log("Listening on port 3000")
+} )
